0a59fb489f8ccb8d1fed5e4688c332e82a8ac264
Naama Aharoni <naamaa@wix.com>
2018-04-10 10:50:02 +0300

    add wixcode-namespaces.js

--- website/scripts/wixcode-namespaces.js
+++ website/scripts/wixcode-namespaces.js
@@ -7,7 +7,7 @@
 		exports["wixcode-namespaces"] = factory(require("wix"), require("_"));
 	else
 		root["wixcode-namespaces"] = factory(root["wix"], root["_"]);
-})(this, function(__WEBPACK_EXTERNAL_MODULE_0__, __WEBPACK_EXTERNAL_MODULE_2__) {
+})(this, function(__WEBPACK_EXTERNAL_MODULE_0__, __WEBPACK_EXTERNAL_MODULE_3__) {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -73,8 +73,202 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 7);
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
 /******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE_0__;
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(true)
+		module.exports = factory(__webpack_require__(0));
+	else if(typeof define === 'function' && define.amd)
+		define(["wix"], factory);
+	else if(typeof exports === 'object')
+		exports["index"] = factory(require("wix"));
+	else
+		root["index"] = factory(root["wix"]);
+})(this, function(__WEBPACK_EXTERNAL_MODULE_0__) {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE_0__;
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _wixcodeSdk = __webpack_require__(0);
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var _core = _extends({}, _wixcodeSdk.core),
+    _module = _core.module;
+
+_module.register('wix-fo', function A() {
+  _classCallCheck(this, A);
+});
+
+/***/ })
+/******/ ]);
+});
+//# sourceMappingURL=index.js.map
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+(function webpackUniversalModuleDefinition(root, factory) {
+  if (true) {module.exports = factory(__webpack_require__(0), __webpack_require__(3));} else if (typeof define === 'function' && define.amd) {define(['wix', '_'], factory);} else if (typeof exports === 'object') {exports['wixcode-users'] = factory(require('wix'), require('_'));} else {root['wixcode-users'] = factory(root.wix, root._);}
+}(this, function(__WEBPACK_EXTERNAL_MODULE_0__, __WEBPACK_EXTERNAL_MODULE_2__) {
+  return /******/ (function(modules) { // webpackBootstrap
+    /******/ 	// The module cache
+    /******/ 	var installedModules = {};
+    /******/
+    /******/ 	// The require function
+    /******/ 	function __webpack_require__(moduleId) {
+      /******/
+      /******/ 		// Check if module is in cache
+      /******/ 		if (installedModules[moduleId])
+      /******/ 			{return installedModules[moduleId].exports;}
+      /******/
+      /******/ 		// Create a new module (and put it into the cache)
+      /******/ 		var module = installedModules[moduleId] = {
+        /******/ 			i: moduleId,
+        /******/ 			l: false,
+        /******/ 			exports: {}
+        /******/ 		};
+      /******/
+      /******/ 		// Execute the module function
+      /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+      /******/
+      /******/ 		// Flag the module as loaded
+      /******/ 		module.l = true;
+      /******/
+      /******/ 		// Return the exports of the module
+      /******/ 		return module.exports;
+      /******/ 	}
+    /******/
+    /******/
+    /******/ 	// expose the modules object (__webpack_modules__)
+    /******/ 	__webpack_require__.m = modules;
+    /******/
+    /******/ 	// expose the module cache
+    /******/ 	__webpack_require__.c = installedModules;
+    /******/
+    /******/ 	// identity function for calling harmony imports with the correct context
+    /******/ 	__webpack_require__.i = function(value) { return value; };
+    /******/
+    /******/ 	// define getter function for harmony exports
+    /******/ 	__webpack_require__.d = function(exports, name, getter) {
+      /******/ 		if (!__webpack_require__.o(exports, name)) {
+        /******/ 			Object.defineProperty(exports, name, {
+          /******/ 				configurable: false,
+          /******/ 				enumerable: true,
+          /******/ 				get: getter
+          /******/ 			});
+        /******/ 		}
+      /******/ 	};
+    /******/
+    /******/ 	// getDefaultExport function for compatibility with non-harmony modules
+    /******/ 	__webpack_require__.n = function(module) {
+      /******/ 		var getter = module && module.__esModule ?
+        /******/ 			function getDefault() { return module.default; } :
+        /******/ 			function getModuleExports() { return module; };
+      /******/ 		__webpack_require__.d(getter, 'a', getter);
+      /******/ 		return getter;
+      /******/ 	};
+    /******/
+    /******/ 	// Object.prototype.hasOwnProperty.call
+    /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+    /******/
+    /******/ 	// __webpack_public_path__
+    /******/ 	__webpack_require__.p = '';
+    /******/
+    /******/ 	// Load entry module and return exports
+    /******/ 	return __webpack_require__(__webpack_require__.s = 5);
+    /******/ }
 /************************************************************************/
 /******/ ([
 /* 0 */
@@ -136,7 +330,7 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
 
 var _consts = __webpack_require__(1);
 
-var _user = __webpack_require__(6);
+var _user = __webpack_require__(4);
 
 var _user2 = _interopRequireDefault(_user);
 
@@ -552,11 +746,6 @@ var Users = function () {
       });
     }
   }, {
-    key: 'naamaTest',
-    value: function naamaTest() {
-      console.log(111111111111);
-    }
-  }, {
     key: 'currentUser',
     get: function get() {
       var RGI = platformUtils.getRGI();
@@ -572,48 +761,6 @@ exports.default = Users;
 
 /***/ }),
 /* 4 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
-var _wixcodeSdk = __webpack_require__(0);
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var _core = _extends({}, _wixcodeSdk.core),
-    _module = _core.module;
-
-_module.register('wix-fo', function A() {
-  _classCallCheck(this, A);
-}, 'foo');
-
-/***/ }),
-/* 5 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
-var _users = __webpack_require__(3);
-
-var _users2 = _interopRequireDefault(_users);
-
-var _wixcodeSdk = __webpack_require__(0);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var _core = _extends({}, _wixcodeSdk.core),
-    _module = _core.module;
-
-_module.register('wix-users', _users2.default, 'user');
-
-/***/ }),
-/* 6 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -696,29 +843,55 @@ var User = function () {
 exports.default = User;
 
 /***/ }),
-/* 7 */
+/* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.Foo = exports.Users = undefined;
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var _Foo = __webpack_require__(4);
+var _users = __webpack_require__(3);
 
-var _Foo2 = _interopRequireDefault(_Foo);
+var _users2 = _interopRequireDefault(_users);
 
-var _Users = __webpack_require__(5);
-
-var _Users2 = _interopRequireDefault(_Users);
+var _wixcodeSdk = __webpack_require__(0);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.Users = _Users2.default;
-exports.Foo = _Foo2.default;
+var _core = _extends({}, _wixcodeSdk.core),
+    _module = _core.module;
+
+_module.register('wix-users', _users2.default);
+
+/***/ })
+/******/ ]));
+}));
+//# sourceMappingURL=wixcode-users.js.map
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE_3__;
+
+/***/ }),
+/* 4 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_wixcode_namespaces_foo_dist_index__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_wixcode_namespaces_foo_dist_index___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_wixcode_namespaces_foo_dist_index__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_wixcode_namespaces_users_dist_wixcode_users__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_wixcode_namespaces_users_dist_wixcode_users___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_wixcode_namespaces_users_dist_wixcode_users__);
+/* harmony reexport (default from non-hamory) */ __webpack_require__.d(__webpack_exports__, "Foo", function() { return __WEBPACK_IMPORTED_MODULE_0_wixcode_namespaces_foo_dist_index___default.a; });
+/* harmony reexport (default from non-hamory) */ __webpack_require__.d(__webpack_exports__, "Users", function() { return __WEBPACK_IMPORTED_MODULE_1_wixcode_namespaces_users_dist_wixcode_users___default.a; });
+
+
+
+
+
 
 /***/ })
 /******/ ]);
